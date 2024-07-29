405d98d824fc0b4d91f4509882ef96e68a41d3f4
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-29 23:28:36 +0300

    commit1

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,10 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+    please pass this test
     </div>
   );
 }
 
 export default Article;
+
